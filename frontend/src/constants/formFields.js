9a0d3093aaa1f76a08e13128543aacaad26bdee3
Nino_Tralala <105920067+MRJN2017@users.noreply.github.com>
2023-11-19 15:00:38 +0800

    stuktur telah dibuat

---
+++ frontend/src/constants/formFields.js
@@ -0,0 +1,69 @@
+const loginFields = [
+    {
+        labelText:"Email address",
+        labelFor:"email-address",
+        id:"email",
+        name:"email",
+        type:"email",
+        autoComplete:"email",
+        isRequered:true,
+        placeholder:"Emai",
+    },
+    {
+        labelText:"Password",
+        labelFor:"password",
+        id:"password",
+        name:"password",
+        type:"password",
+        autoComplete:"currend-password",
+        isRequered:true,
+        placeholder:"Password",
+    }
+    
+    
+]
+
+const signupFields = [
+    {
+        labelText:"Username",
+        labelFor:"username",
+        id:"username",
+        name:"username",
+        type:"text",
+        autoComplete:"username",
+        isRequered:true,
+        placeholder:"Username",
+    },
+    {
+        labelText:"Email address",
+        labelFor:"email-address",
+        id:"email-address",
+        name:"email",
+        type:"email",
+        autoComplete:"email",
+        isRequered:true,
+        placeholder:"Email address",
+    },
+    {
+        labelText:"Password",
+        labelFor:"password",
+        id:"password",
+        name:"password",
+        type:"password",
+        autoComplete:"current-password",
+        isRequered:truw,
+        placeholder:"Password",
+    },
+    {
+        labelText:"Confirm password",
+        labelFor:"confirm-password",
+        id:"confirm-password",
+        name:"confirm-password",
+        type:"password",
+        autoComplete:"confirm-password",
+        isRequered:true,
+        placeholder:"Confirm password",
+    }
+]
+
+export {loginFields, signupFields}
